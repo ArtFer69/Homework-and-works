12ca5772b1933750eef9785430ad6714651d911e
ArtFer69 <79011523+ArtFer69@users.noreply.github.com>
2021-03-06 07:25:23 -0600

    Examen final

---
+++ prueba/Assets/Scripts/Agente dinamico/agentedinamico.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class agentedinamico : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    public Transform[] waypoint;
+    public int indexWaypoint = 0;
+   
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.SetDestination(waypoint[indexWaypoint].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.remainingDistance == 0)
+        {
+            indexWaypoint = (indexWaypoint + 1) % waypoint.Length;
+            agent.SetDestination(waypoint[indexWaypoint].position);
+        }
+    }
+}
